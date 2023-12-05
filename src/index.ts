00086c4be90a45e57fe0c5871441106da4d863d8
Joseph-01 <softjoe40@gmail.com>
2023-12-05 09:48:13 +0100

    working on the crawler from scratch again

--- src/index.ts
+++ src/index.ts
@@ -2,48 +2,12 @@ import * as cheerio from "cheerio";
 import axios from "axios";
 
 
-// Get current date
-var today = new Date();
-
-// Define days of the week
-var daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-// Get the day of the week (0-6)
-var dayOfWeek = today.getDay();
-
-// Get day, month, and year
-var day = today.getDate();
-var month = today.getMonth() + 1; // Months are zero-indexed, so add 1
-var year = today.getFullYear();
-
-// Function to get the month name
-function getMonthName(month) {
-  var monthNames = [
-    'January', 'February', 'March', 'April', 'May', 'June',
-    'July', 'August', 'September', 'October', 'November', 'December'
-  ];
-  return monthNames[month - 1];
-}
-
-var formattedDate = daysOfWeek[dayOfWeek]
-var formattedmonth =  getMonthName(month)
-var formattedDay = day;
-var formattedYear = year;
-
-
-
-
-const url: string =
-  `https://www.thedevotionals.com.ng/rhapsody-of-realities-for-${formattedDate.toLocaleLowerCase()}-${formattedDay}th-${formattedmonth.toLocaleLowerCase()}-2023`;
+const url: string = `https://www.thedevotionals.com.ng/devotional/rhapsody-of-realities/`;
 
 async function crawler(url) {
-  const response = await axios.get(url);
-  const html = response.data;
-  const $ = cheerio.load(html);
-  $("p", html).each(function () {
-    const tes = $(this).text();
-    console.log(tes);
-  });
+  const response = await axios.get(url)
+  const html = await response.data
+  console.log(html);
 }
 
 crawler(url)
